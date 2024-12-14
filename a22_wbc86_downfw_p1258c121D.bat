63a7d9902dd7cfddf07010cd195697cd1dde6090
STwirelesscharger <will.zhou@st.com>
2024-12-14 13:54:36 +0800

    update gui to 2.9 version

---
+++ a22_wbc86_downfw_p1258c121D.bat
@@ -0,0 +1,6 @@
+@echo off
+REM use tool and command line to down fw to chip
+set memh_patch="STSW-WBC86FWBPP\STEVAL-WBC86TX_nvmpatch_1258.memh"
+set memh_cfg="STSW-WBC86FWBPP\STEVAL-WBC86TX_config_121D_BPP.memh"
+"STSW-WPSTUDIO V2.2.5.exe" fw -d WBC86 -p %memh_patch% -c %memh_cfg%
+pause
